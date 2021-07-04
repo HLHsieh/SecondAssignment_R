ff66452ddfede52b8d39da05daf18f89eb988244
HHL <hsieh.332@ous.edu>
2021-07-04 02:18:27 +0000

    Update Argument

--- SecondAssignment_R.r
+++ SecondAssignment_R.r
@@ -1,11 +1,13 @@
 #### Rscript SecondAssignment_R.r regrex1.csv
 
-## read arguement
-args<-commandArgs(T)
+
+## read argument
+args <- commandArgs(T)
 
 
 ## load data
 df <- read.csv(args[1], head = TRUE)
+
 
 ## plot scatter
 png("r_orig.png")
@@ -15,12 +17,10 @@ Null <- dev.off()
 
 ## model the data
 linear_regressor <- lm(y ~ x, data = df)
-# summary(linear_regressor)
-# Y_pred <- predict(linear_regressor, data = data.frame(df$x))
 
 
 ## plot scatter with predicting value
 png("r_lm.png")
 plot(y ~ x, data = df)
-abline(linear_regressor, col  = "red")
-Null <-dev.off()
+abline(linear_regressor, col = "red")
+Null <- dev.off()
